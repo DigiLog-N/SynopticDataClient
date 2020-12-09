55c58fb970dfb4e44f6d02ab22e991a7611dc801
Charles Cowart <charlie@canvasslabs.com>
2020-12-09 11:40:34 -0800

    Adding Apache 2.0 License to files
    Added a proper LICENSE.txt file containing the full license, and a shortened header version at the top of each file.
    Attribution given for code clips taken from PyMOTW and Stack Overflow.

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ import socket
 import sys
 
 # stock UDP server
-# (taken from https://pymotw.com/2/socket/udp.html)
+# (taken from https://pymotw.com/2/socket/udp.html, Copyright Doug Hellman)
+
+# this stock UDP server was used to test the receiving of data from the
+# client. In practice, other components will be receiving this data instead.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('127.0.0.1', 8888)
